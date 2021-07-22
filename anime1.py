33ee95fa6f8421f139d964deb8b5c52d165515cf
HeiTang <heitang512@purr.tw>
2021-07-22 19:35:17 +0800

    feat: Anime_Season

--- anime1.py
+++ anime1.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
 import requests,os,sys,re
 import datetime, json
 from bs4 import BeautifulSoup
+
 Cookies = None
-download_path = os.getcwd() + "/Anime1_Download"
+download_path = "{}/Anime1_Download".format(os.getcwd())
 
 # 設定 Header 
 headers = {
@@ -17,38 +17,46 @@ headers = {
     "cookie": "__cfduid=d8db8ce8747b090ff3601ac6d9d22fb951579718376; _ga=GA1.2.1940993661.1579718377; _gid=GA1.2.1806075473.1579718377; _ga=GA1.3.1940993661.1579718377; _gid=GA1.3.1806075473.1579718377",
     "Content-Type":"application/x-www-form-urlencoded",
     "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3573.0 Safari/537.36",
-    }
+}
+
+def Anime_Season(url):
+    URLS = []
+    # https://anime1.me/category/.../...
+    r = requests.post(url, headers = headers)
+    soup = BeautifulSoup(r.text, 'lxml') 
+    h2 = soup.find_all('h2', class_="entry-title")
+    for i in h2:
+        url = i.find("a", attrs={"rel": "bookmark"}).get('href')
+        URLS.append(url)
+    
 
 
-def Anime_Unit(url):
-    #1 https://anime1.me/14323
-    r = requests.post(url,headers = headers)
+def Anime_Episode(url):
+    #1 https://anime1.me/...
+    r = requests.post(url, headers = headers)
     soup = BeautifulSoup(r.text, 'lxml') 
     url = soup.find('iframe').get('src')
     title = soup.find('h1', class_="entry-title").text
-    # print(title)
 
-    #2 https://v.anime1.me/watch?v=KRPeC
+    #2 https://v.anime1.me/watch?v=...
     r = requests.post(url,headers = headers)
     soup = BeautifulSoup(r.text, 'lxml') 
     script_text = soup.find_all("script")[1].string
-    xsend = 'd=' + re.search(r"'d=(.*?)'", script_text, re.M|re.I).group(1)
+    xsend = 'd={}'.format(re.search(r"'d=(.*?)'", script_text, re.M|re.I).group(1))
 
     #3 APIv2
     r = requests.post('https://v.anime1.me/api',headers = headers,data = xsend)
-    url = 'https:' + json.loads(r.text)['l']
+    url = 'https:{}'.format(json.loads(r.text)['l'])
     
     global Cookies
     set_cookie = r.headers['set-cookie']
-    cookie_e = 'e=' + re.search(r"e=(.*?);", set_cookie, re.M|re.I).group(1) + ';'
-    cookie_p = 'p=' + re.search(r"p=(.*?);", set_cookie, re.M|re.I).group(1) + ';'
-    cookie_h = 'h=' + re.search(r"HttpOnly, h=(.*?);", set_cookie, re.M|re.I).group(1) + ';'
-    Cookies = cookie_e + cookie_p + cookie_h
-    # print(Cookies)
+    cookie_e = re.search(r"e=(.*?);", set_cookie, re.M|re.I).group(1)
+    cookie_p = re.search(r"p=(.*?);", set_cookie, re.M|re.I).group(1)
+    cookie_h = re.search(r"HttpOnly, h=(.*?);", set_cookie, re.M|re.I).group(1)
+    Cookies = 'e={};p={};h={};'.format(cookie_e, cookie_p, cookie_h)
     MP4_DL(url, title)
 
-def MP4_DL(Doenload_URL, Video_Name):
-
+def MP4_DL(Download_URL, Video_Name):
     headers_cookies ={
         "accept": "*/*",
         "accept-encoding": 'identity;q=1, *;q=0',
@@ -57,21 +65,33 @@ def MP4_DL(Doenload_URL, Video_Name):
         "dnt": '1',
         "user-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
     }
-
-    r = requests.get(Doenload_URL,headers = headers_cookies) 
-    with open(os.path.join(download_path,  Video_Name + ".mp4"), 'wb') as f:
-        f.write(r.content)
-        f.flush()
-        f.close()
-
+    
+    r = requests.get(Download_URL, headers = headers_cookies) 
     if(r.status_code == 200):
-        print("\033[11D\033[0m",end= "", flush= True )
-        print("\033[1;34mSuccess    \033[0m")  # 藍色成功
-    else:
-        print("\033[1;31mFailure    \033[0m")  # 紅色錯誤
+        with open(os.path.join(download_path,  '{}.mp4'.format(Video_Name)), 'wb') as f:
+            f.write(r.content)
+            f.flush()
+            f.close()
 
-if __name__ == '__main__': 
+        print("\033[1;34mSuccess\033[0m") 
+    else:
+        print("\033[1;31mFailure\033[0m：{}".format(r.status_code)) 
+
+if __name__ == '__main__':     
     if not os.path.exists(download_path):
         os.mkdir(download_path)
 
-    Anime_Unit("https://anime1.me/15606")
+    anime_url = input("Anime1 URL：")
+    # anime_url = "https://anime1.me/15606"
+    # anime_url = "https://anime1.me/category/2021%e5%b9%b4%e6%98%a5%e5%ad%a3/edens-zero"
+    
+    # 區分連結類型
+    if re.search(r"anime1.me/category/(.*?)", anime_url, re.M|re.I):
+        Anime_Season(anime_url)
+    elif re.search(r"anime1.me/[0-9]", anime_url, re.M|re.I):
+        Anime_Episode(anime_url)
+    else:
+        print("I don't like this link. QAQ")
+
+    
+    
